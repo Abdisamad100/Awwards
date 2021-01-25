75e16c187952a62ab764954632d3384d86abac7e
abdisamad100 <abdisamadcade004@gmail.com>
2021-01-25 12:25:26 +0300

    added class projects to models

--- appawward/models.py
+++ appawward/models.py
@@ -1,37 +1,45 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
-from pyuploadcare.dj.models import ImageField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(max_length=300)
-    technology = models.CharField(max_length=300,blank=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="posts")
-    date = models.DateTimeField(auto_now_add=True,blank=True)
-    url = models.URLField(max_length=300)
-    photo = ImageField(manual_crop='1280x720')
-    
+class Image(models.Model):
+    image = CloudinaryField('images')
+    description = models.CharField(max_length=100,blank=True)
 
     def __str__(self):
-        return f"{self.title}"
+        return self.description
 
-    def delete_post(self):
-        return self.delete
+class Profile(models.Model):
+    profile_photo = CloudinaryField('profpics')
+    bio = models.TextField(blank = True)
+    user_id = models.OneToOneField(User, null=True,on_delete=models.CASCADE)
 
-    def save_post(self):
-        return self.save()
+    def __str__(self):
+        return self.bio
 
-    @classmethod
-    def all_post(cls):
-        return cls.objects.all()
+    def save_user(self):
+        self.save()
 
-    @classmethod
-    def get_languages(cls,language):
-        language = Post.objects.filter(language=language).all()
-        return language
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user_id=instance)
 
-    @classmethod
-    def search_project(cls, title):
-        return cls.objects.filter(title__icontains=title).all()
+    @receiver(post_save,sender=User)
+    def save_profile(sender,instance, **kwargs):
+        instance.profile.save()
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=50, blank=True)
+    project_photo = CloudinaryField('projectpics/')
+    description = models.TextField(max_length=400,blank=True)
+    github_repo = models.CharField(max_length=150, blank=True)
+    url = models.CharField(max_length=60,blank=True)
+    uploader = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url
