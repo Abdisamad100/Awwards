80d7b42ed9807f809609a75bd25c9a0b4f816f05
abdisamad100 <abdisamadcade004@gmail.com>
2021-01-25 12:32:32 +0300

    added home url

--- awwards/urls.py
+++ awwards/urls.py
@@ -13,12 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from appawward import views
+from django.contrib.auth import views as auth_views
+from rest_framework.authtoken.views import obtain_auth_token
 
-from django.conf.urls import url,include
+
+
+app_name='ratings'
 
 urlpatterns = [
-   
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^', include('appawward.urls')),
+    url(r'^accounts/logout/', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
+    # url(r'^logout/$', views.logout, name='logout'), # To be implemented later 
+    # url(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
+    url(r'^api-token-auth/', obtain_auth_token),
+    url(r'^search/$', views.search, name='search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
