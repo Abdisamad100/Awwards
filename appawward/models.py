fd5d8ce5b9df411c033eb264815fa0a8e96cd591
abdisamad100 <abdisamadcade004@gmail.com>
2021-01-26 22:11:03 +0300

    changed uploader to charField

--- appawward/models.py
+++ appawward/models.py
@@ -39,7 +39,7 @@ class Projects(models.Model):
     description = models.TextField(max_length=400,blank=True)
     github_repo = models.CharField(max_length=150, blank=True)
     url = models.CharField(max_length=60,blank=True)
-    uploader = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
+    uploader = models.CharField(max_length=40,blank=True)
 
     def __str__(self):
         return self.url
